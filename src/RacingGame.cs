0ec499f94803a2c64f53b631f52457eda00ed697
Antoine Busque <antoinebusque@gmail.com>
2012-02-01 09:19:47 -0500

    Instanciation des objets dans RacingGame

--- src/RacingGame.cs
+++ src/RacingGame.cs
@@ -33,11 +33,37 @@ namespace RaceXNA
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
+            graphics.SynchronizeWithVerticalRetrace = false;
+            IsFixedTimeStep = false;
+            IsMouseVisible = false;
+            graphics.IsFullScreen = false;
         }
 
         protected override void Initialize()
         {
+            FontMgr = new ResourceManager<SpriteFont>(this);
+            TextureMgr = new ResourceManager<Texture2D>(this);
+            ModelMgr = new ResourceManager<Model>(this);
+
+            LoadAssets();
+
+            FpsHandler = new FpsCounter(this, FPS_INTERVAL);
+            FpsDisplayer = new FpsDisplay(this, "Pericles20");
+            InputMgr = new InputManager(this);
+            GameCamera = new FreeCamera(this, Vector3.Zero, Vector3.Zero, Vector3.Up);
+
+            Components.Add(FpsHandler);
+            Components.Add(InputMgr);
+            Components.Add(GameCamera);
+            //Laisser FpsDisplayer a la fin de la liste pour eviter les problemes d'affichage
+            Components.Add(FpsDisplayer);
+
             base.Initialize();
+        }
+
+        private void LoadAssets()
+        {
+            FontMgr.Add("Fonts/Pericles20");
         }
 
         protected override void LoadContent()
@@ -58,11 +84,24 @@ namespace RaceXNA
             base.Update(gameTime);
         }
 
+        protected override bool BeginDraw()
+        {
+            GraphicsDevice.Clear(Color.Black);
+            spriteBatch.Begin();
+
+            return base.BeginDraw();
+        }
+
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
-
             base.Draw(gameTime);
+        }
+
+        protected override void EndDraw()
+        {
+            spriteBatch.End();
+
+            base.EndDraw();
         }
     }
 }
