de6911e587c82b7f17f469042d8db436e73045d9
Antoine Busque <antoinebusque@gmail.com>
2012-02-06 23:46:09 -0500

    Acceleration and rotation now works both forward and back, with friction too.

--- src/Vehicle.cs
+++ src/Vehicle.cs
@@ -10,13 +10,14 @@ namespace RaceXNA
     {
         const float MAX_ACCEL = 100.0f;
         const float BASE_ACCEL = 7.0f;
-        const float FRICTION = 2.0f;
+        const float FRICTION = 2.5f;
 
         const float BASE_ROT = 0.75f;
 
         public Vector3 Acceleration { get; private set; }
         public Vector3 Velocity { get; private set; }
         public ChasingCamera Camera { get; private set; }
+        public bool GoingForward { get; private set; }
 
         public Vehicle(RacingGame raceGame, String modelName, Vector3 initPos, float initScale, Vector3 initRot)
             : base(raceGame, modelName, initPos, initScale, initRot)
@@ -24,28 +25,16 @@ namespace RaceXNA
             Acceleration = Vector3.Zero;
             Velocity = Vector3.Zero;
             Camera = new ChasingCamera(this);
+            GoingForward = true;
         }
 
         public override void Update(GameTime gameTime)
         {
-            Vector3 backward = World.Backward;
-            backward.Normalize();
 
-            HandleInput();
-            if (Acceleration.Length() > 0)
-            {
-                Acceleration += backward * FRICTION;
-            }
+            HandleAcceleration();
 
-            Velocity += Acceleration / RaceGame.FpsHandler.FpsValue;
-            if (Velocity.Z < 0)
-            {
-                Velocity = Vector3.Zero;
-                Acceleration = Vector3.Zero;
-            }
 
-            //J'ai fait mes changements dans une nouvelle méthode temporairement 
-            //pour pas que tu sois mélanger quand tu voudras modifier l'accélération
+
             HandleRotation();
 
             Position -= Velocity / RaceGame.FpsHandler.FpsValue;
@@ -53,7 +42,7 @@ namespace RaceXNA
             base.Update(gameTime);
         }
 
-        private void HandleInput()
+        private void HandleAcceleration()
         {
             Vector3 forward = World.Forward;
             forward.Normalize();
@@ -63,19 +52,47 @@ namespace RaceXNA
             if (rightTriggerValue > 0.0f)
             {
                 Acceleration += forward * BASE_ACCEL * rightTriggerValue;
+                GoingForward = true;
             }
-            if (leftTriggerValue > 0.0f)
+            else if (leftTriggerValue > 0.0f)
             {
                 Acceleration -= forward * BASE_ACCEL * leftTriggerValue;
+                GoingForward = false;
+            }
+
+            if (Acceleration.Length() > 0.0f)
+            {
+                if (GoingForward)
+                    Acceleration -= forward * FRICTION;
+                else
+                    Acceleration += forward * FRICTION;
+            }
+
+            Velocity += Acceleration / RaceGame.FpsHandler.FpsValue;
+            if (GoingForward && Vector3.Dot(forward, Velocity) < 0)
+            {
+                Velocity = Vector3.Zero;
+                Acceleration = Vector3.Zero;
+            }
+            else if(!GoingForward && Vector3.Dot(forward, Velocity) > 0)
+            {
+                Velocity = Vector3.Zero;
+                Acceleration = Vector3.Zero;
             }
         }
 
         private void HandleRotation()
         {
+            if(Velocity.Length() < 0.5f)
+                return;
+
             float leftThumbStickHorizontalValue = -RaceGame.InputMgr.ControllerState.ThumbSticks.Left.X;
             float yaw;
 
             yaw = BASE_ROT * leftThumbStickHorizontalValue / RaceGame.FpsHandler.FpsValue;
+
+            if (!GoingForward)
+                yaw = -yaw;
 
             Rotation = new Vector3(Rotation.X, Rotation.Y + yaw, Rotation.Z);
         }
