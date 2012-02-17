f4d6234f77a562a0fb8d672c7da4d158e0557bc4
Antoine Busque <antoinebusque@gmail.com>
2012-02-17 09:05:29 -0500

    Ajout de fonction pour lire le fichier image de terrain

--- src/Terrain.cs
+++ src/Terrain.cs
@@ -21,19 +21,22 @@ namespace RaceXNA
         private Vector2 Size { get; set; }
         private Vector2 TileSize { get; set; }
         private TerrainTile[,] TerrainTiles { get; set; }
+        private Texture2D MapTexture { get; set; }
 
-        public Terrain(Game game, Vector3 origin, Vector2 size, Vector2 dimension)
+        public Terrain(Game game, Vector3 origin, Texture2D mapTexture)
             : base(game)
         {
             Origin = origin;
-            Size = size;
-            Dimension = dimension;
+            MapTexture = mapTexture;
+            Size = new Vector2(MapTexture.Width, MapTexture.Height);
+            Dimension = new Vector2(MapTexture.Width, MapTexture.Height);
             TileSize = new Vector2(Size.X / Dimension.X, Size.Y / Dimension.Y);
         }
 
         public override void Initialize()
         {
             TerrainTiles = new TerrainTile[(int)Dimension.X, (int)Dimension.Y];
+            CreateFromTexture();
 
             base.Initialize();
         }
@@ -46,6 +49,23 @@ namespace RaceXNA
             }
 
             base.Draw(gameTime);
+        }
+
+        private void CreateFromTexture()
+        {
+            Color[][] pixelColors = new Color[MapTexture.Width][];
+            for(int i = 0; i < MapTexture.Width; ++i)
+                pixelColors[i] = new Color[MapTexture.Height];
+            Rectangle sourceRectangle;
+
+            for (int i = 0; i < MapTexture.Width; ++i)
+            {
+                for (int j = 0; j < MapTexture.Height; ++j)
+                {
+                    sourceRectangle = new Rectangle(i, j, 1, 1);
+                    MapTexture.GetData<Color>(0, sourceRectangle, pixelColors[i], j, 1);
+                }
+            }
         }
     }
 }
