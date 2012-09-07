f79975ac5cdff8cba489e9b2e7f8dfa41ed01251
Philippe Lacasse-Labelle <PhilippeLacasseLabelle@hotmail.com>
2012-09-07 14:39:08 -0400

    Ajout de collisions fonctionnelles

--- src/BaseObject.cs
+++ src/BaseObject.cs
@@ -120,6 +120,11 @@ namespace RaceXNA
           return Boxes.Count;
       }
 
+      public int GetSpheresLenght()
+      {
+          return Spheres.Length;
+      }
+
       public override void Update(GameTime gameTime)
       {
          World = Matrix.Identity * Matrix.CreateScale(Scale);
